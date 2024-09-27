dec862a3180476f059c4c8c9a20a17d6f46c29c0
indroji-mondal <indro.cse.bu@gmail.com>
2024-09-27 14:49:52 +0600

    updated with renaming

--- find.js
+++ find.js
@@ -2,5 +2,5 @@ const friends =['Titu','Kisaloy','Hironmoy','Shimul','Khalil'];
 const newFriends = friends.find(x => x==='Hironmoy');
 console.log(newFriends);
 const numbers =[33,50,79,78,90,101,30];
-const oddNumbers = numbers.find( x => x%10==0);
-console.log(oddNumbers);
+const firstOddNumber = numbers.find( x => x%10==0);
+console.log(firstOddNumber);
