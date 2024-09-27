1d74a2c686b5de0847a3dcf89935ba7d1f048e15
indroji-mondal <indro.cse.bu@gmail.com>
2024-09-27 13:57:50 +0600

    solved practise.js

--- destructor.js
+++ destructor.js
@@ -8,8 +8,3 @@ console.log(price);
 const [first,,third]=[4,5,6];
 console.log(first);
 console.log(third);
-console.log(Math.max([4,5,6])); // reference error 
-const array=[4,5,6];
-console.log(Math.max(...array)); 
-function min(nums) { return Math.min(...nums) }
-console.log(min( [11,5,6,8 ]));
---
+++ find.js
@@ -0,0 +1,3 @@
+const friends =['Titu','Kisaloy','Hironmoy','Shimul','Khalil'];
+const newFriends = friends.find(x => x==='Hironmoy');
+console.log(newFriends);
